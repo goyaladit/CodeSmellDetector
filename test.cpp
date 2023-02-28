a3b81f70a492885b58b79e075817ee63a181088c
goyaladit <80494397+goyaladit@users.noreply.github.com>
2023-02-27 19:08:01 -0800

    Added list of tasks to do

---
+++ test.cpp
@@ -0,0 +1,39 @@
+/* Adit Goyal
+ * CPSC 5910 - Refactoring
+ *
+ * List of Tasks:
+ * - Finish "List of Tasks"
+ * - define data structure for holding
+ *         > functions in a file
+ *              > code block for each function (lines of code)
+ *              > parameter list of each function
+ *              > return type of function
+ *              > metrics related to each function (i.e.
+ *              LOC, etc.)
+ *         > a file or class
+ *              > should hold functions in it
+ *
+ * - code for storing a test file as a data structure obj
+ * - figure out metric for similarity between code fragments
+ * - code for UI
+ *         > Choosing detection of long method, long param list,
+ *         or duplicated code
+ *         > back to main menu after completing an action (loop)
+ *         > option to 'Quit' in main menu
+ * - code to handle invalid input (ask the prof. what is considered
+ * invalid input)
+ *
+ *
+ *
+
+ */
+using namespace std;
+#include <iostream>
+
+int main (int argc, char ** argv)
+{
+
+    cout << "HI" << endl;
+
+    return 0;
+}
