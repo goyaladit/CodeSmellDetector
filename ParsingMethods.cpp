668b518a78958f8551c7f4b861130d2eb8c8b3e5
goyaladit <80494397+goyaladit@users.noreply.github.com>
2023-03-02 02:49:29 -0800

    Got the initialization of a FileData object working, and printing of function names working

--- ParsingMethods.cpp
+++ ParsingMethods.cpp
@@ -8,6 +8,8 @@
 using namespace std;
 #include <iostream>
 #include <fstream>
+#include <regex>
+#include <string>
 #include "FileData.h"
 #include "FunctionData.h"
 
@@ -16,6 +18,34 @@ class ParsingMethods
 
 
     public:
+
+        static list<string> getListOfFuncNames() {
+            list<string> funcNames;
+            fstream testFile;
+            //ios:in for read operation
+            testFile.open("TestFile.cpp", ios::in);
+            if (testFile.is_open()) {
+                string temp;
+                while (getline(testFile, temp)) {
+                    regex firstLineOfFuncRegex("^[^\\s]\\w+\\s+\\w+\\([\\s\\S]*\\)[\\s\\S]*\\{");
+                    if (regex_search(temp, firstLineOfFuncRegex)) { //regex matched
+                        //string temp contains first line of func
+                        //code for pulling out func name
+                        //ASSUMPTION - function first line is exactly of this form: "int fun1(int a, int b, int c, int d){"
+                        int indexOfFirstSpace = temp.find(' ');
+                        int indexOfParenthesis = temp.find('(');
+                        string funcName = temp.substr(indexOfFirstSpace + 1, indexOfParenthesis-indexOfFirstSpace-1);
+                        funcNames.push_back(funcName);
+
+                        //*add func name to funcNames list
+                    }
+
+                }
+            }
+
+            return funcNames;
+        }
+
         static void printLineByLine()
         {
             fstream testFile;
@@ -26,7 +56,17 @@ class ParsingMethods
                 string temp;
                 while(getline(testFile, temp))
                 {
-                    cout << temp << endl;
+
+                    cout << "WORKS!!" << endl;
+
+
+                    regex b("^[^\\s]\\w+\\s+\\w+\\([\\s\\S]*\\)[\\s\\S]*\\{");
+                    if(regex_search(temp, b))
+                    {
+                        cout << "Regex matched with the following: " << endl;
+                        cout << temp << endl << endl << endl;
+                    }
+
                 }
             }
 
@@ -34,3 +74,18 @@ class ParsingMethods
         }
 
 };
+
+
+/*
+* Regex Notes:
+* [^\s] = no whitespace
+* ^ = beginning
+* //w = word (letters and numbers)
+* + = one or more
+* "^[^\\s]\\w+\\s+\\w+\\(" = matches "int main("
+ * [\s\S] = matches any character
+ * ^[^\s]\w+\s+\w+\([\s\S]*\)[\s\S]*{ = matches "int main() {"
+ *
+ *
+ *
+*/
